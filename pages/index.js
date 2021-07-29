c2ed16a94753d824b59e7766b1984e48f6d405ab
hadi-a-azeez <hadikoduvally@gmail.com>
2021-07-29 19:43:58 +0530

    added animation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import Head from "next/head";
 //mport Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import NavBar from "../components/NavBar";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +21,7 @@ export default function Home() {
       <NavBar />
       <main className={styles.main}>
         <div className={styles.wraper_row}>
-          <div className={styles.grid_left}>
+          <div className={styles.grid_left} data-aos="fade-up">
             <h1 className={styles.heading_main}>
               TikTok videos on your shopify website
             </h1>
@@ -24,7 +31,7 @@ export default function Home() {
             </h1>
             <button className={styles.btn_main}>Install on Shopify</button>
           </div>
-          <div className={styles.grid_right}>
+          <div className={styles.grid_right} data-aos="fade-right">
             <video
               alt="vid"
               muted
